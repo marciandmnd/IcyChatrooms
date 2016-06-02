c5da91529e93aaacb623b525c7a3584f9451cd49
marciandmnd <marciandmnd@yahoo.com>
2016-06-01 20:08:47 -0700

    fixed syntax error in node server listen function

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ function serveStatic(response, cache, absPath) {
   }
 }
 var port = process.env.PORT || 5000;
-server.listen(process.env.PORT3000, function(){
+server.listen(port, function(){
   console.log("Server listening on port " + port + '.');
 });
 
